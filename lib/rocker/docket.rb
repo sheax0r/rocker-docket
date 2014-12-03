1cce2ab4ae8d8caa8b3931bf1d07bea315df1181
Mike Shea <michael.shea@salesforce.com>
2014-12-02 20:39:53 -0500

    Initial commit

---
+++ lib/rocker/docket.rb
@@ -0,0 +1,7 @@
+require "rocker/docket/version"
+
+module Rocker
+  module Docket
+    # Your code goes here...
+  end
+end
---
+++ lib/rocker/docket/version.rb
@@ -0,0 +1,5 @@
+module Rocker
+  module Docket
+    VERSION = "0.0.1"
+  end
+end
